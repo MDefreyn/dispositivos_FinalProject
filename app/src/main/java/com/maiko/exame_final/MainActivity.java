a299a78372898033b3d1e9e0ec2a39319f5c3945
MDefreyn <mdefreyn65@gmail.com>
2021-05-23 23:22:11 -0300

    Ajuste Activity, filtro de datas

--- app/src/main/java/com/maiko/exame_final/MainActivity.java
+++ app/src/main/java/com/maiko/exame_final/MainActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -20,6 +24,8 @@ import com.maiko.exame_final.model.Conta;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
@@ -29,11 +35,15 @@ public class MainActivity extends AppCompatActivity
     Connection bd;
     ContaDAO contasDAO;
 
-    ListView lVwContas;
+    EditText eTxFiltro;
     Button btnAdicionar;
+    ListView lVwContas;
 
     ArrayAdapter<Conta> adapter;
-    List<Conta> listaContas;
+    ArrayList<Conta> listaContas;
+
+    Calendar calendar = Calendar.getInstance();
+    DatePickerDialog.OnDateSetListener dPDialog;
     SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy", new Locale("pt", "BR"));
     DecimalFormat df = new DecimalFormat("0.00");
 
@@ -42,18 +52,63 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        lVwContas = findViewById(R.id.lVwContas);
+        eTxFiltro = findViewById(R.id.eTxFiltro);
         btnAdicionar = findViewById(R.id.btnAdicionar);
+        lVwContas = findViewById(R.id.lVwContas);
 
         bd = Room.databaseBuilder(getApplicationContext(), Connection.class, "lista_contas").allowMainThreadQueries().build();
         contasDAO = bd.getContaDAO();
 
         listarContas();
+
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listaContas);
         lVwContas.setAdapter(adapter);
         lVwContas.setOnItemLongClickListener(this);
+        dPDialog = (view, year, month, dayOfMonth) -> {
+            calendar.set(Calendar.YEAR, year);
+            calendar.set(Calendar.MONTH, month);
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            atualizaCampo();
+        };
+        eTxFiltro.setOnClickListener(v -> new DatePickerDialog(MainActivity.this, dPDialog,
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)).show());
+        eTxFiltro.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
 
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                filtrar();
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
         btnAdicionar.setOnClickListener(v -> adicionar());
+    }
+
+    private void filtrar() {
+        Date filtrado = calendar.getTime();
+        Date conta;
+        ArrayList<Conta> lstFiltrados = new ArrayList<>();
+        listarContas();
+        for (int i = 0; i < listaContas.size(); i++) {
+            conta = listaContas.get(i).getVencimento();
+            if (filtrado.compareTo(conta) >= 0) {
+                lstFiltrados.add(listaContas.get(i));
+            }
+        }
+        listaContas.clear();
+        listaContas.addAll(lstFiltrados);
+    }
+
+    private void atualizaCampo() {
+        eTxFiltro.setText(sdf.format(calendar.getTime()));
     }
 
     public void adicionar() {
